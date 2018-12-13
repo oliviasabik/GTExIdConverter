4ff27309ff7547dbde2a20414137c1f561b06766
ols5fg <ols5fg@virginia.edu>
2018-12-13 15:09:06 -0500

    Fixing merge

--- R/GTExIdConvert.R
+++ R/GTExIdConvert.R
@@ -19,17 +19,46 @@ GTExIdConvert <- function(variant_ids){
   }
 
   #detect which chr are in the variant ids
-  #pull data from github for variant ids
-  myfile <- "https://raw.github.com/oliviasabik/GTExVariantIdData/master/chr_1_gtex_ids.txt"
-  chr_1 <- readr::read_tsv(myfile, col_names = FALSE)
-
-  #make the variant id input a data_frame, one per chromosome
   a = data.frame(variant_ids)
+  colnames(a) = "variant_id"
+  b = data.frame(do.call('rbind', strsplit(as.character(a$variant_id),'_',fixed=TRUE)))
+  b = cbind(a,b)
+  colnames(b) = c("variant_id", "chr", "position", "allele1", "allele2", "build")
+  list_c = as.vector(unique(b$chr))
+  list_c
+  #pull data from github for variant ids
+  if(length(list_c) == 1){
+    myfile <- paste0("https://raw.github.com/oliviasabik/GTExVariantIdData/master/chr_", list_c[1], "_gtex_ids.txt")
+  }else if(length(list_c) > 1){
+    myfile <- paste0("https://raw.github.com/oliviasabik/GTExVariantIdData/master/chr_", list_c[1], "_gtex_ids.txt")
+    for(i in 2:length(list_c)){
+      myfile = c(myfile, paste0("https://raw.github.com/oliviasabik/GTExVariantIdData/master/chr_", list_c[i], "_gtex_ids.txt"))
+    }
+  }
 
-  #merge the dataframe with the variant ids by chromosome
-  b = merge(a, )
-
+  for(i in 1:length(myfile)){
+    assign(paste0("chr_", list_c[i]), readr::read_tsv(myfile[1], col_names = FALSE))
+  }
+  #make the variant id input a data_frame, one per chromosome and merge
+  c_list = vector(mode="character", length=length(list_c))
+  for(i in 1:length(list_c)){
+    print(i)
+    c_list[i] = paste0("chr_", list_c[i])}
+  for (i in 1:length(c_list)){
+    curr_c = gsub("chr_", "", c_list[i])
+    c = subset(b, b$chr == as.numeric(curr_c))
+    d = get(c_list[1])
+    colnames(d) = c("variant_id", "rs_id")
+    e = merge(c, d, by = "variant_id", all.x = TRUE)
+    e = e[,c(1,7)]
+    assign(paste0("data_", curr_c), e)
+  }
   #combine merged chromosomes
-
-  #return data
+  d_list = vector(mode="character", length=length(list_c))
+  for(i in 1:length(list_c)){
+    print(i)
+    d_list[i] = paste0("data_", list_c[i])}
+ f = rbind(get(d_list))
+ #return data
+ return(f)
 }
